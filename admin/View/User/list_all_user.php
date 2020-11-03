9a1b0f7421fe82cc668ecde09ab0cdadcce6ebe5
confusing1997 <luuhainam.it@gmail.com>
2020-11-03 21:01:51 +0700

    Make Directory User in View

---
+++ admin/View/User/list_all_user.php
@@ -0,0 +1,26 @@
+
+
+<form action="" method="POST">
+    <div class="form-group">
+        <input type="text" class="form-control" id="key_search_user" name="key_search_user">
+    </div>
+</form>
+
+<div class="notification"></div>
+
+<div class="table-responsive">
+  <table class="table table-hover">
+      <thead>
+          <tr>
+              <th>STT</th>
+              <th>Họ tên</th>
+              <th>Showroom</th>
+              <th>Email</th>
+              <th>Action</th>
+          </tr>
+      </thead>
+
+      <tbody id="view_all_user"></tbody>
+      
+  </table>
+</div>
