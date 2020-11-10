ef979767a7dde378d2504d41ee533b3340636cbc
confusing1997 <luuhainam.it@gmail.com>
2020-11-10 18:38:33 +0700

    Create file to handle update method

--- admin/View/User/list_all_user.php
+++ admin/View/User/list_all_user.php
@@ -1,7 +1,7 @@
 <div class="notification"></div>
 
 <!-- BEGIN ADD USER MODAL -->
-<button class="btn btn-icon waves-effect waves-light btn-purple addUser" 
+<button type="button" class="btn btn-icon waves-effect waves-light btn-purple addUser" 
 data-toggle = "modal" data-target = "#add_user" title="Sửa">
      <i class="fa fa-plus" aria-hidden="true"></i> Thêm nhân viên
 </button>
@@ -89,12 +89,85 @@ data-toggle = "modal" data-target = "#add_user" title="Sửa">
                       <td><?= $valueUser['name'] ?></td>
                       <td><?= $valueUser['title'] ?></td>
                       <td><?= $valueUser['email'] ?></td>
-                      <td class="text-center">
-                          <button class="btn btn-icon waves-effect waves-light btn-warning" title="Sửa">
+                      <td class="">
+
+                          <!-- BEGIN EDIT USER MODAL -->
+
+                          <button class="btn btn-icon waves-effect waves-light btn-warning"
+                          data-toggle = "modal"
+                          data-target = "#edit_user<?= $valueUser['id']; ?>"
+                          data-whatever = "@getbootstrap"
+                          title="Sửa">
                             <i class="fas fa-wrench"></i>
                           </button>
-                          <button class="btn btn-danger btn-icon waves-effect waves-light removeUser" value="<?= $valueUser['id']; ?>">  
-                            <i class="fas fa-times" title="Xóa"></i>
+
+                          <div class="modal fade" id="edit_user<?= $valueUser['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="exampleModalLabel">Sửa thông tin nhân viên</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body">
+                                  <form>
+                                    <div class="form-group">
+                                      <label for="name" class="col-form-label">Tên nhân viên</label>
+                                      <input type="text" class="form-control" id="staff_name" name="user_name" value="<?= $valueUser['name']; ?>">
+                                    </div>
+
+                                    <div class="form-group">
+                                      <label for="name" class="col-form-label">Showroom</label>
+                                      <select name="" id="staff_showroom" class="form-control">
+                                        <?php 
+                                          foreach($showroom as $valueShowroom) {
+                                        ?>
+                                          <option value="<?= $valueShowroom['showroom_id']; ?>"><?= $valueShowroom['title'] ?></option>
+                                        <?php 
+                                          } 
+                                        ?>
+                                      </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                      <label for="phone" class="col-form-label">Email</label>
+                                      <input type="text" class="form-control" id="staff_email" name="user_email" value="<?= $valueUser['email']; ?>">
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                      <label for="email" class="col-form-label">Avatar</label>
+                                      <input type="text" class="form-control" id="avatar_user" name="avatar_user" value="<?= $valueUser['avatar']; ?>">
+                                    </div>
+
+                                    <div class="form-group">
+                                      <label for="email" class="col-form-label">Địa chỉ</label>
+                                      <input type="text" class="form-control" id="staff_address" name="user_address" value="<?= $valueUser['addres']; ?>">
+                                    </div>
+
+                                    <div class="form-group">
+                                      <label for="email" class="col-form-label">Lương cơ bản</label>
+                                      <input type="text" class="form-control" id="staff_salary" name="user_salary" value="<?= $valueUser['salary']; ?>">
+                                    </div>
+
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                                      <button type="submit" class="btn btn-primary edit_user" value="<?= $valueUser['id']; ?>">Sửa</button>
+                                    </div>
+                                    
+                                  </form>
+                                </div>
+                                
+                              </div>
+                            </div>
+                          </div>
+
+                          <!-- END EDIT USER MODAL -->
+
+                          <button class="btn btn-danger btn-icon waves-effect waves-light removeUser" 
+                          value="<?= $valueUser['id']; ?>" 
+                          title="Xóa">  
+                            <i class="fas fa-times"></i>
                           </button>
                       </td>    
                   </tr>
