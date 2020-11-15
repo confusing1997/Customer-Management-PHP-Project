f3d3719d17dda716cb31cf37328560c4e8ebe33e
confusing1997 <luuhainam.it@gmail.com>
2020-11-15 21:22:12 +0700

    Create Ajax Controller for Server folder

---
+++ admin/Controller/Product/Product_c_ajax.php
@@ -0,0 +1,26 @@
+<?php 
+
+    include_once("../../Model/Product/Product_m_ajax.php");
+
+    class Product_c_ajax extends Product_m_ajax {
+
+        private $product;
+
+        function __construct()
+        {
+            $this->product = new Product_m_ajax();
+        }
+
+        public function addProduct ($name, $price, $description) {
+
+            return $this->product->addProduct($name, $price, $description);
+
+        }
+
+        public function removeProduct($id) {
+
+            return $this->product->removeProduct($id);
+
+        }
+
+    }
