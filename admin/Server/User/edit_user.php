aed9293f99c47cae7eb00194cdec3fd8e36869f7
confusing1997 <luuhainam.it@gmail.com>
2020-11-29 21:12:51 +0700

    Output error if code run wrong

--- admin/Server/User/edit_user.php
+++ admin/Server/User/edit_user.php
@@ -13,25 +13,63 @@
         $address = trim($_POST['address']);
         $salary = trim($_POST['salary']);
 
-        $editUser = $user->editUser ($id, $name, $avatar, $email, $address, $salary);
+        $num = count($user->checkEmailUser($email));
 
-        if ($editUser) :
+        if ($num == 0 && $name != '' && $avatar != '' && $email != '' && $address != '' && $salary != ''):
+
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)):
+
+                $editUser = $user->editUser ($id, $name, $avatar, $email, $address, $salary);
+
+                if ($editUser):
 ?>
 
-            <div class="alert alert-success">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <strong>Update Successfully!</strong> 
-            </div>
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <strong>Update Successfully!</strong> 
+                </div>
 
 <?php 
+                endif;
 
-        endif;
+            else:
 ?>
+
+                <div class="alert alert-warning">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <strong>Email is Invalid!</strong> 
+                </div>
+
+<?php
+            endif;
+
+        elseif ($name =='' || $avatar == '' || $email == '' || $address == '' || $salary ==''):
+            
+?>
+            <div class="alert alert-warning">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <strong>Data must not be Empty!</strong> 
+            </div>
+
+<?php
+        else:
+?>
+
+            <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <strong>Staff is already exists!</strong> 
+            </div>
+
+<?php
+        endif;
+
+?>
+        
 
 <script type="text/javascript">
     //Hiện thông báo .. giây xong ẩn
     $(document).ready(function(){
-        $(".alert").delay(2000).slideUp();
+        $(".alert").delay(1000).slideUp();
     })
 </script>
         
