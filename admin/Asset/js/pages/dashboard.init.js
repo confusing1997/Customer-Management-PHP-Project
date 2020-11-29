f1daac30af5f5be696fb28d9bafed4f2843d7d72
confusing1997 <luuhainam.it@gmail.com>
2020-11-29 21:10:45 +0700

    Change chart template

--- admin/Asset/js/pages/dashboard.init.js
+++ admin/Asset/js/pages/dashboard.init.js
@@ -3,9 +3,14 @@
     var sh1 = document.getElementById('sh1_cus').value;
     var sh2 = document.getElementById('sh2_cus').value;
     var sh3 = document.getElementById('sh3_cus').value;
+
     var sh1_avg = document.getElementById('sh1_cus_avg').value;
     var sh2_avg = document.getElementById('sh2_cus_avg').value;
     var sh3_avg = document.getElementById('sh3_cus_avg').value;
+
+    var sh1_staff = document.getElementById('sh1_staff_am').value;
+    var sh2_staff = document.getElementById('sh2_staff_am').value;
+    var sh3_staff = document.getElementById('sh3_staff_am').value;
     var e=function(){}
     
     e.prototype.respChart=function(e,r,a,t){
@@ -60,15 +65,15 @@
                 
                 ticks:{max:30,min:0,stepSize:5}}]}});this.respChart(i("#sales-history"),"Bar",
                 
-                {labels:["January","February","March","April","May","June","July"],
+                {labels:["Showroom 1","Showroom 2","Showroom 3"],
                 
-                datasets:[{label:"Sales Analytics",backgroundColor:"#3ec396",borderColor:"#3ec396",borderWidth:1,hoverBackgroundColor:"#3ec396",hoverBorderColor:"#3ec396",
+                datasets:[{label:"Resources Analytics",backgroundColor:"#3ec396",borderColor:"#3ec396",borderWidth:1,hoverBackgroundColor:"#3ec396",hoverBorderColor:"#3ec396",
                 
-                data:[65,59,80,81,56,89,40,20]}]},
+                data:[sh1_staff,sh2_staff,sh3_staff]}]},
                 
                 {scales:{yAxes:[{gridLines:{color:"rgba(255,255,255,0.05)",fontColor:"#fff"},
                 
-                ticks:{max:100,min:20,stepSize:20}}],xAxes:[{gridLines:{color:"rgba(0,0,0,0.1)"}}]}})},
+                ticks:{max:20,min:0,stepSize:5}}],xAxes:[{gridLines:{color:"rgba(0,0,0,0.1)"}}]}})},
                 
                 i.ChartJs=new e,i.ChartJs.Constructor=e}(window.jQuery),
                 
