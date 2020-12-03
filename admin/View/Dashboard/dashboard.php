189265b659a59ced6d1b038661c298ead86688d1
confusing1997 <luuhainam.it@gmail.com>
2020-12-03 08:57:39 +0700

    Change template of dashboard

--- admin/View/Dashboard/dashboard.php
+++ admin/View/Dashboard/dashboard.php
@@ -115,7 +115,7 @@
 						<div class="col-sm-4">
 							<div class="text-center mt-3">
 								<h6 class="font-normal text-muted font-14">Product Amount</h6>
-								<h6 class="font-18"><i class="fa fa-university" aria-hidden="true"></i> 
+								<h6 class="font-18"><i class="fa fa-briefcase" aria-hidden="true"></i> 
 								<span class="text-dark"><?= $amountOfProduct; ?></span> </h6>
 							</div>
 						</div>
@@ -148,7 +148,7 @@
 
 			<div class="col-md-6 col-xl-3">
 				<div class="card-box tilebox-one">
-					<i class="fe-box float-right"></i>
+					<i class="fa fa-comments float-right"></i>
 					<h5 class="text-muted text-uppercase mb-3 mt-0">Care Content</h5>
 					<h3 class="mb-3" data-plugin="counterup"><?= $personalAmount; ?></h3>
 					<!-- <span class="badge badge-primary"> +11% </span> 
@@ -158,7 +158,7 @@
 
 			<div class="col-md-6 col-xl-3">
 				<div class="card-box tilebox-one">
-					<i class="fe-layers float-right"></i>
+					<i class="fa fa-university float-right"></i>
 					<h5 class="text-muted text-uppercase mb-3 mt-0">Bonus</h5>
 					<h3 class="mb-3"><span data-plugin="counterup"><?= number_format($sumBonus); ?></span> VND</h3>
 					<!-- <span class="badge badge-primary"> -29% </span> 
@@ -168,7 +168,7 @@
 
 			<div class="col-md-6 col-xl-3">
 				<div class="card-box tilebox-one">
-					<i class="fe-tag float-right"></i>
+					<i class="fa fa-briefcase float-right"></i>
 					<h5 class="text-muted text-uppercase mb-3 mt-0">Orders</h5>
 					<h3 class="mb-3"><span data-plugin="counterup"><?= $personalProductAmount; ?></span> </h3>
 					<!-- <span class="badge badge-primary"> 0% </span> 
@@ -178,98 +178,32 @@
 
 			<div class="col-md-6 col-xl-3">
 				<div class="card-box tilebox-one">
-					<i class="fe-briefcase float-right"></i>
+					<i class="fa fa-user float-right"></i>
 					<h5 class="text-muted text-uppercase mb-3 mt-0">Customer</h5>
 					<h3 class="mb-3" data-plugin="counterup"><?= $personalCustomerBeingCare; ?></h3>
 					<!-- <span class="badge badge-primary"> +89% </span> <span class="text-muted ml-2 vertical-middle">Last year</span> -->
 				</div>
 			</div>
 
-		</div>
-
-	
-
-		<div class="row">
-			<!-- Start System Overview -->
-			
-			<div class="col-xl-7">
-				<div class="card-box">
-					<h4 class="header-title">Overview</h4>
-
-					<div class="row">
-						<div class="col-sm-4">
-							<div class="text-center mt-3">
-								<h6 class="font-normal text-muted font-14">Total Customer</h6>
-								<h6 class="font-18"><i class="fa fa-users" aria-hidden="true"></i> 
-								<span class="text-dark"><?= $personalTotalCustomer; ?></span> </h6>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="text-center mt-3">
-								<h6 class="font-normal text-muted font-14">Number of Transference</h6>
-								<h6 class="font-18"><i class="fa fa-paper-plane" aria-hidden="true"></i> 
-								<span class="text-dark"><?= $personalTransference; ?></span> </h6>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="text-center mt-3">
-								<h6 class="font-normal text-muted font-14">Total Care Content</h6>
-								<h6 class="font-18"><i class="fa fa-keyboard"></i> 
-								<span class="text-dark"><?= $contentAmount; ?></span> </h6>
-							</div>
-						</div>
-					</div>
-					
-					<canvas id="transactions-chart" height="350" class="mt-4"></canvas>
-					
-					<!-- System Overview to Dashboard.init.js -->
-					
-					<!-- System Overview to Dashboard.init.js -->
-					
+			<div class="col-md-6 col-xl-3">
+				<div class="card-box tilebox-one">
+					<i class="fa fa-users float-right"></i>
+					<h5 class="text-muted text-uppercase mb-3 mt-0">Total Customer</h5>
+					<h3 class="mb-3" data-plugin="counterup"><?= $personalTotalCustomer; ?></h3>
+					<!-- <span class="badge badge-primary"> +89% </span> <span class="text-muted ml-2 vertical-middle">Last year</span> -->
 				</div>
-				
 			</div>
-			
-			<!-- End System Overview -->
 
-			<!-- Start Resources Overview -->
-			<!-- <div class="col-xl-5">
-				<div class="card-box">
-					<h4 class="header-title">Resources Overview</h4>
-
-					<div class="row">
-						<div class="col-sm-4">
-							<div class="text-center mt-3">
-								<h6 class="font-normal text-muted font-14">Total Admin</h6>
-								<h6 class="font-18 "><i class="fa fa-user-circle"></i> 
-								<span class="text-dark"><?= $amountOfAdmin ?></span> <small></small></h6>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="text-center mt-3">
-								<h6 class="font-normal text-muted font-14">Product Amount</h6>
-								<h6 class="font-18"><i class="fa fa-university" aria-hidden="true"></i> 
-								<span class="text-dark"><?= $amountOfProduct; ?></span> </h6>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="text-center mt-3">
-								<h6 class="font-normal text-muted font-14">Total Staff</h6>
-								<h6 class="font-18"><i class="fa fa-address-card"></i> 
-								<span class="text-dark"><?= $amountOfStaff; ?></span> </h6>
-							</div>
-						</div>
-					</div>
-
-					<canvas id="sales-history" height="350" class="mt-4"></canvas>
-
-					Resources Overview to Dashboard.init.js -->
-					
-					<!-- Resources Overview to Dashboard.init.js
+			<div class="col-md-6 col-xl-3">
+				<div class="card-box tilebox-one">
+					<i class="fa fa-paper-plane float-right"></i>
+					<h5 class="text-muted text-uppercase mb-3 mt-0">Transference Amount</h5>
+					<h3 class="mb-3" data-plugin="counterup"><?= $personalTransference; ?></h3>
+					<!-- <span class="badge badge-primary"> +89% </span> <span class="text-muted ml-2 vertical-middle">Last year</span> -->
 				</div>
-			</div> -->
-			<!-- End Resources Overview -->	
-			
+			</div>
+
+
 		</div>
 	
 	<?php } ?>
